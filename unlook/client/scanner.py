a93ae794c7c4e90357352c321d8d7dc8246f8145
SupernovaIndustries <alessandro.cursoli@supernovaindustries.it>
2025-05-06 18:24:17 +0200

    Update scanner.py

--- unlook/client/scanner.py
+++ unlook/client/scanner.py
@@ -28,21 +28,6 @@ from ..common.events import UnlookClientEvent
 logger = logging.getLogger(__name__)
 
 
-class UnlookClientEvent(enum.Enum):
-    """Eventi generati dal client UnLook."""
-    CONNECTED = "connected"
-    DISCONNECTED = "disconnected"
-    RECONNECTING = "reconnecting"
-    SCANNER_FOUND = "scanner_found"
-    SCANNER_LOST = "scanner_lost"
-    ERROR = "error"
-    STREAM_STARTED = "stream_started"
-    STREAM_STOPPED = "stream_stopped"
-    SCAN_STARTED = "scan_started"
-    SCAN_COMPLETED = "scan_completed"
-    SCAN_ERROR = "scan_error"
-
-
 class UnlookClient:
     """
     Client principale per la connessione a uno scanner UnLook.
